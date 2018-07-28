98a98bcf351d54ec54d46fc7e3526b2cd697cf55
matukai <matt.tanji@gmail.com>
2018-07-27 15:00:32 -1000

    post on users  route working

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,30 @@
 const express = require('express');
-const Users = require('../db/models/Users')
 const router = express.Router();
-
+const Users = require('../db/models/Users')
 
 router.route('/')
 .get((req,res)=>{
-  console.log('hitting route')
   return Users
   .fetchAll()
   .then(result=>{
     return res.json(result)
   })
 })
+.post((req,res) => {
+  return new Users ({
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  })
+  .save()
+  .then(newUser => {
+    return res.json(newUser);
+  })
+  .catch(err => {
+    res.json({message: err})
+  })
+})
+
+
 
 module.exports = router;
