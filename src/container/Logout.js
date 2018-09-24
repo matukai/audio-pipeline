4ffefd6eb21fe8f1fee78fa9d163524e67668a28
matukai <matt.tanji@gmail.com>
2018-09-24 13:18:06 -1000

    login & logout

--- src/container/Logout.js
+++ src/container/Logout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Logout extends Component {
   constructor(props) {
@@ -7,6 +8,7 @@ class Logout extends Component {
 
   onClick() {
     localStorage.clear();
+    axios.get('/api/logout');
   }
 
   render() {
